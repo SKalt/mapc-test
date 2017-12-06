e98007aaa986c97fcbcc85f29d11761a4a34035f
Unknown <kalt.steven@gmail.com>
2017-12-05 22:30:45 -0500

    Added webpack config

---
+++ frontend/poll/webpack.config.js
@@ -0,0 +1,44 @@
+/* global __dirname*/
+const path = require('path');
+const webpack = require('webpack');
+// const CleanWebpackPlugin = require('clean-webpack-plugin');
+const fs = require('fs');
+
+module.exports = (env={}) => {
+  return {
+    entry: './src/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'dist')
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            'css-loader'
+          ]
+        },
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          use: [
+            'file-loader'
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new webpack.DefinePlugin(Object.assign({ // defaults
+        ENV: '"debugging"',
+        MB_ACCESS_TOKEN: fs.readFileSync('api-keys/mapbox.txt', 'utf8').trim(),
+        MZ_API_KEY: fs.readFileSync('api-keys/mapzen.txt', 'utf8').trim(),
+      }, env)),
+      // new CleanWebpackPlugin(['dist']),
+    ],
+  };
+};
