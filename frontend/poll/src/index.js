19566ac3d2bfa430d72991ce5d31f81d90ff8c77
Unknown <kalt.steven@gmail.com>
2017-12-05 22:57:36 -0500

    Added submit button and split out updateCoordinates
    I split out updateCoordinates for readability and to create an internal coordinates variable to act as the coordinates state.

--- frontend/poll/src/index.js
+++ frontend/poll/src/index.js
@@ -1,43 +1,23 @@
-/* global L */
+/* global L, MB_ACCESS_TOKEN, MZ_API_KEY */
 // set up the map
 // state and map will be globals for the moment for ease of development
 import {point} from '@turf/helpers';
 import * as wfst from 'geojson-to-wfs-t-2';
+import {getCoordinates, updateCoordinates} from './updateCoordinates.js';
 
 var coordinates;
-const map = new L.map('map').setView([-71, 42], 13);
+const map = new L.map('map').setView([ 42.36, -71.0589], 13);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
   maxZoom: 18,
   id: 'mapbox.streets',
-  accessToken: 'MB_ACCESS_TOKEN'
+  accessToken: MB_ACCESS_TOKEN
 }).addTo(map);
 
-const geocoder = L.control.geocoder('MZ_API_KEY', {
+const geocoder = L.control.geocoder(MZ_API_KEY, {
   placeholder: 'Search your place of residence'
 });
-/**
- * Updates the response coordinates;
- * @param  {Number[]|Object} lngLatLike a wgs84 coordinate array or {lng, lat}
- * @return undefined
- */
-function updateCoordinates(lngLatLike=[]){
-  let [lng, lat] = lngLatLike;
-  const check = (...args) => args.every(
-    n => n && n.consructor == Number && 180 > n > -180
-  );
-  if (check(lng, lat)){
-    coordinates = lngLatLike;
-  } else {
-    let {lng, lat} = lngLatLike;
-    if (check(lng, lat)){
-      coordinates = lngLatLike;
-    } else {
-      throw new Error('unexpected input', lngLatLike);
-    }
-  }
-}
 
 map.on('click', e => updateCoordinates(e.lnglat));
 geocoder.on('select', e => updateCoordinates(e.feature.geometry.coordinates));
@@ -73,3 +53,7 @@ function submit(){
     alert('Please enter both coordinates and a response to submit your answer');
   }
 }
+
+const button = document.getElementById('submit');
+button.addEventListener('click', submit);
+button.disabled = false;
